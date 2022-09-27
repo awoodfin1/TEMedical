b244834912f81deddc55f48e0abcd1ce724b9e0d
Aaron Woodfin <awoodfin1@gmail.com>
2022-09-27 00:26:06 -0400

    Created Appointment and Patient models

---
+++ capstone/java/src/main/java/com/techelevator/model/Appointment.java
@@ -0,0 +1,107 @@
+package com.techelevator.model;
+
+import java.sql.Time;
+import java.time.LocalDate;
+
+public class Appointment {
+
+    private int id;
+    private int patientId;
+    private int providerId;
+    private LocalDate appointmentDate;
+    private Time apptStartTime;
+    private Time apptEndTime;
+    private char status;            //Appointment is (confirmed, rescheduled, cancelled, completed).
+    private String appointmentReason;
+    private String appointmentDetails;
+
+    public Appointment(){
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public int getProviderId() {
+        return providerId;
+    }
+
+    public void setProviderId(int providerId) {
+        this.providerId = providerId;
+    }
+
+    public LocalDate getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(LocalDate appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public Time getApptStartTime() {
+        return apptStartTime;
+    }
+
+    public void setApptStartTime(Time apptStartTime) {
+        this.apptStartTime = apptStartTime;
+    }
+
+    public Time getApptEndTime() {
+        return apptEndTime;
+    }
+
+    public void setApptEndTime(Time apptEndTime) {
+        this.apptEndTime = apptEndTime;
+    }
+
+    public char getStatus() {
+        return status;
+    }
+
+    public void setStatus(char status) {
+        this.status = status;
+    }
+
+    public String getAppointmentReason() {
+        return appointmentReason;
+    }
+
+    public void setAppointmentReason(String appointmentReason) {
+        this.appointmentReason = appointmentReason;
+    }
+
+    public String getAppointmentDetails() {
+        return appointmentDetails;
+    }
+
+    public void setAppointmentDetails(String appointmentDetails) {
+        this.appointmentDetails = appointmentDetails;
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment{" +
+                "id=" + id +
+                ", patientId=" + patientId +
+                ", providerId=" + providerId +
+                ", appointmentDate=" + appointmentDate +
+                ", apptStartTime=" + apptStartTime +
+                ", apptEndTime=" + apptEndTime +
+                ", status=" + status +
+                ", appointmentReason='" + appointmentReason + '\'' +
+                ", appointmentDetails='" + appointmentDetails + '\'' +
+                '}';
+    }
+}
---
+++ capstone/java/src/main/java/com/techelevator/model/Patient.java
@@ -0,0 +1,55 @@
+package com.techelevator.model;
+
+public class Patient {
+
+    private int id;
+    private int userId;
+    private String firstName;
+    private String lastName;
+
+    public Patient() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
