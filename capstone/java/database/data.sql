641f6046dac594c9a458c3e306979a7c91edda5e
Trey Thompson <trey.thompson413@gmail.com>
2022-09-29 11:05:19 -0500

    Reworked database files. Everyone needs to recreate the DB at this moment.

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -1,19 +1,39 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO patient (user_id, first_name, last_name) VALUES (1, 'testFirstA', 'testLastA');
+INSERT INTO users (user_id,username,password_hash,role,is_provider)
+	VALUES
+(100001,'user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER', false);
 
-INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
-INSERT INTO provider (user_id, first_name, last_name, title) VALUES (2, 'testFirstB', 'testLastB', 'M.D.');
+INSERT INTO patient (patient_id, user_id, first_name, last_name, phone_number, email_address, birthdate, health_issues_description)
+	VALUES
+		(2001, 100001, 'patientAFirst', 'patientALast', '8005551234', 'first.last@gmail.com', '1994-01-01', 'Insert health issues desc. here');
 
-INSERT INTO users (username,password_hash,role) VALUES ('user2', 'password_hash', 'ROLE_USER');
-INSERT INTO patient (user_id, first_name, last_name) VALUES (3, 'first', 'last');
+INSERT INTO users (user_id,username,password_hash,role, is_provider)
+	VALUES
+	(100002,'admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN', true);
+	
+INSERT INTO provider (provider_id, user_id, title, first_name, last_name, post_nominals, gender, language, rating, phone_number, bio, photo_URL)
+	VALUES
+		(1001, 100002, 'Dr.', 'providerAFirst', 'providerALast', 'MD', 'Male', 'English', 4.55, '8009994567', 'Insert provider bio here.', 'https://as2.ftcdn.net/v2/jpg/03/20/52/31/1000_F_320523164_tx7Rdd7I2XDTvvKfz2oRuRpKOPE5z0ni.jpg');
 
-INSERT INTO users (username,password_hash,role) VALUES ('admin2', 'password_hash', 'ROLE_ADMIN');
-INSERT INTO provider (user_id, first_name, last_name, title) VALUES (4, 'firstProvider', 'lastProvider', 'D.O.');
+INSERT INTO appointment (patient_id, provider_id, appointment_date, appt_start_time, appt_end_time)
+	VALUES (2001, 1001, '2022-10-31', '12:00:00', '12:30:00');
 
-INSERT INTO appointments (patient_id, provider_id, appointment_date, appt_start_time, appt_end_time) VALUES (1, 1, '2022-10-31', '09:00:00', '09:30:00');
+INSERT INTO office (office_id, office_name, tag_line, street_address, city, "state", zipcode, office_open_time, office_close_time, phone_number, cost_per_hour, has_appt_delay)
+	VALUES
+	(1, 'Memorial Hermann Physician Group', 'Quality Care, Quality Health', '1011 Washington St', 'Chicago', 'IL', 60018, '09:00:00', '17:00:00','8889794545',20.00, false);
 
-INSERT INTO office (office_id, street_address, city, state, zipcode, office_open_time, office_close_time, phone_number, cost_per_hour, has_appt_delay) VALUES (1, '1011 Washington St', 'Chicago', 'IL', 60018, '09:00:00', '17:00:00','8889794545',20.00, false);
+INSERT INTO specialty (field_name)
+	VALUES ('Oncology');
+
+INSERT INTO review (office_id, patient_id, provider_id, review_text, review_date, review_time, provider_rating, provider_response_text, response_date, response_time)
+	VALUES
+		(1, 2001, 1001, 'Patient provides review text here.', '2022-09-27', '10:30:00', 5, 'Provider provides review response text here.', '2022-09-28', '13:45:00');
+
+INSERT INTO provider_office (provider_id, office_id)
+	VALUES (1001, 1);
+
+INSERT INTO provider_specialty (provider_id, specialty_id)
+	VALUES (1001, 101);
 
 COMMIT TRANSACTION;
--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,129 +1,169 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS patient CASCADE;
-DROP TABLE IF EXISTS provider CASCADE;
-DROP TABLE IF EXISTS office CASCADE;
-DROP TABLE IF EXISTS provider_office CASCADE;
-DROP TABLE IF EXISTS reviews CASCADE;
-DROP TABLE IF EXISTS responses CASCADE;
-DROP TABLE IF EXISTS appointments CASCADE;
+DROP TABLE IF EXISTS users, patient, provider, appointment, office, review, specialty, provider_office, provider_specialty CASCADE;
 
-DROP SEQUENCE IF EXISTS user_serial;
-DROP SEQUENCE IF EXISTS patient_serial;
-DROP SEQUENCE IF EXISTS provider_serial;
-DROP SEQUENCE IF EXISTS appointment_serial;
-DROP SEQUENCE IF EXISTS office_serial;
-DROP SEQUENCE IF EXISTS review_serial;
-DROP SEQUENCE IF EXISTS response_serial;
+DROP SEQUENCE IF EXISTS user_serial, patient_serial, provider_serial, appointment_serial, office_serial, review_serial, specialty_serial CASCADE;
 
-CREATE SEQUENCE user_serial;
+-- Sequence to start user_id values at 100,001 instead of 1
+CREATE SEQUENCE user_serial
+  INCREMENT BY 1
+  START WITH 100001
+  NO MAXVALUE;
+  
 CREATE TABLE users (
 	user_id int NOT NULL default nextval ('user_serial'),
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
-	role varchar(50) NOT NULL,
+	"role" varchar(50) NOT NULL,
+	is_provider boolean,
 
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
-CREATE SEQUENCE patient_serial;
+-- Sequence to start patient_id values at 2,001 instead of 1
+CREATE SEQUENCE patient_serial
+  INCREMENT BY 1
+  START WITH 2001
+  NO MAXVALUE;
+  
 CREATE TABLE patient (
-             patient_id int NOT NULL default nextval ('patient_serial'),
-             user_id int,
-             first_name varchar(20),
-             last_name varchar(30),
+    patient_id int NOT NULL default nextval ('patient_serial'),
+    user_id int,
+    first_name varchar(30) NOT NULL,
+	last_name varchar(40) NOT NULL,
+	phone_number varchar(10) NOT NULL,
+	email_address varchar(60) NOT NULL,
+	birthdate date NOT NULL,
+	health_issues_description varchar(500),
 
-             CONSTRAINT pk_patient PRIMARY KEY (patient_id),
-             CONSTRAINT fk_patient FOREIGN KEY (user_id) REFERENCES users(user_id)
+    CONSTRAINT pk_patient PRIMARY KEY (patient_id),
+	CONSTRAINT fk_patient FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-CREATE SEQUENCE provider_serial;
+-- Sequence to start patient_id values at 1,001 instead of 1
+CREATE SEQUENCE provider_serial
+  INCREMENT BY 1
+  START WITH 1001
+  NO MAXVALUE;
+  
 CREATE TABLE provider (
-             provider_id int NOT NULL default nextval ('provider_serial'),
-             user_id int,
-             first_name varchar(20),
-             last_name varchar(30),
-             title varchar(15),
-
-             CONSTRAINT pk_provider PRIMARY KEY (provider_id),
-             CONSTRAINT fk_provider FOREIGN KEY (user_id) REFERENCES users(user_id)
+	provider_id int NOT NULL default nextval ('provider_serial'),
+	user_id int,
+	title varchar(4),
+    first_name varchar(30),
+    last_name varchar(40),
+	post_nominals varchar(20),
+	gender varchar(6),
+	"language" varchar(15),
+	rating numeric(4,2),
+	phone_number varchar(10),
+	bio varchar(500),
+	photo_URL varchar(200),	
+    
+    CONSTRAINT pk_provider PRIMARY KEY (provider_id),
+    CONSTRAINT fk_provider FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-CREATE SEQUENCE office_serial;
-CREATE TABLE office (
-             office_id int NOT NULL default nextval ('office_serial'),
-             street_address varchar(50),
-             city varchar(40),
-             state char(2),
-             zipcode int,
-             office_open_time TIME,
-             office_close_time TIME,
-             phone_number varchar(10),
-             cost_per_hour NUMERIC(4,2),
-             has_appt_delay boolean,
+-- Sequence to start appointment_id values at 1001 instead of 1
+CREATE SEQUENCE appointment_serial
+  INCREMENT BY 1
+  START WITH 1001
+  NO MAXVALUE;
+  
+CREATE TABLE appointment (
+	appointment_id int NOT NULL default nextval ('appointment_serial'),
+    patient_id int NOT NULL,
+    provider_id int NOT NULL,
+    appointment_date DATE NOT NULL,
+    appt_start_time TIME NOT NULL,
+    appt_end_time TIME,
+    status varchar(15) DEFAULT ('Confirmed'),
+    appointment_reason varchar(70),
+    appointment_details varchar(100),
 
-             CONSTRAINT pk_office PRIMARY KEY (office_id)
+    CONSTRAINT pk_appointment PRIMARY KEY (appointment_id),
+    CONSTRAINT fk_appointment_patient_id FOREIGN KEY (patient_id) REFERENCES patient(patient_id),
+    CONSTRAINT fk_appointment_provider_id FOREIGN KEY (provider_id) REFERENCES provider(provider_id)
+);
+
+-- Sequence to start office_id values at 1
+CREATE SEQUENCE office_serial
+  INCREMENT BY 1
+  START WITH 1
+  NO MAXVALUE;
+  
+CREATE TABLE office (
+    office_id int NOT NULL default nextval ('office_serial'),
+	office_name varchar (75) NOT NULL,
+	tag_line varchar (100),
+    street_address varchar(50),
+    city varchar(40),
+    "state" char(2),
+    zipcode int,
+    office_open_time time,
+    office_close_time time,
+    phone_number varchar(10),
+    cost_per_hour numeric(4,2),
+    has_appt_delay boolean NOT NULL default (false),
+
+    CONSTRAINT pk_office PRIMARY KEY (office_id)
+);
+
+-- Sequence to start review_id values at 1,001 instead of 1
+CREATE SEQUENCE review_serial
+  INCREMENT BY 1
+  START WITH 1001
+  NO MAXVALUE;
+
+CREATE TABLE review (
+	review_id int NOT NULL default nextval ('review_serial'),
+    office_id int,
+    patient_id int,
+	provider_id int,
+    review_text varchar(500),
+	review_date date,
+    review_time time,
+    provider_rating int,
+	provider_response_text varchar(500),
+	response_date date,
+	response_time time,
+
+    CONSTRAINT pk_review PRIMARY KEY (review_id),
+    CONSTRAINT fk_review_office_id FOREIGN KEY (office_id) REFERENCES office(office_id),
+    CONSTRAINT fk_review_patient_id FOREIGN KEY (patient_id) REFERENCES patient(patient_id),
+	CONSTRAINT fk_review_provider_id FOREIGN KEY (provider_id) REFERENCES provider(provider_id)
+);
+
+-- Sequence to start specialty_id values at 101 instead of 1
+CREATE SEQUENCE specialty_serial
+  INCREMENT BY 1
+  START WITH 101
+  NO MAXVALUE;
+  
+CREATE TABLE specialty (
+	specialty_id int default nextval ('specialty_serial'),
+	field_name varchar(60),
+	
+	CONSTRAINT PK_specialty PRIMARY KEY (specialty_id)
 );
 
 CREATE TABLE provider_office (
-             provider_id int NOT NULL UNIQUE,
-             office_id int NOT NULL,
+	provider_id int NOT NULL UNIQUE,
+    office_id int NOT NULL,
 
-			 CONSTRAINT pk_provider_office PRIMARY KEY(provider_id, office_id),
-             CONSTRAINT fk_provider_office_provider_id FOREIGN KEY (provider_id) REFERENCES provider(provider_id),
-             CONSTRAINT fk_provider_office_office_id FOREIGN KEY (office_id) REFERENCES office(office_id)
+	CONSTRAINT pk_provider_office PRIMARY KEY(provider_id, office_id),
+    CONSTRAINT fk_provider_office_provider_id FOREIGN KEY (provider_id) REFERENCES provider(provider_id),
+    CONSTRAINT fk_provider_office_office_id FOREIGN KEY (office_id) REFERENCES office(office_id)
 );
 
-CREATE SEQUENCE review_serial;
-CREATE TABLE reviews (
-             review_id int NOT NULL default nextval ('review_serial'),
-             office_id int,
-             patient_id int,
-             review_text varchar(500),
-             review_time TIMESTAMP,
-             rating int,
-
-             CONSTRAINT pk_reviews PRIMARY KEY (review_id),
-             CONSTRAINT fk_reviews_office_id FOREIGN KEY (office_id) REFERENCES office(office_id),
-             CONSTRAINT fk_reviews_patient_id FOREIGN KEY (patient_id) REFERENCES patient(patient_id)
+CREATE TABLE provider_specialty (
+	provider_id int NOT NULL UNIQUE,
+	specialty_id int NOT NULL,
+	
+	CONSTRAINT PK_provider_specialty PRIMARY KEY(provider_id, specialty_id),
+	CONSTRAINT FK_provider_specialty_provider_id FOREIGN KEY (provider_id) REFERENCES provider(provider_id),
+	CONSTRAINT FK_provider_specialty_specialty_id FOREIGN KEY (specialty_id) REFERENCES specialty(specialty_id)
 );
-
-CREATE SEQUENCE response_serial;
-CREATE TABLE responses (
-             response_id int NOT NULL default nextval ('response_serial'),
-             review_id int,
-             provider_id int,
-             response_text varchar(500),
-             response_time TIMESTAMP,
-
-             CONSTRAINT pk_responses PRIMARY KEY (response_id),
-             CONSTRAINT fk_responses_review_id FOREIGN KEY (review_id) REFERENCES reviews(review_id),
-             CONSTRAINT fk_responses_provider_id FOREIGN KEY (provider_id) REFERENCES provider_office(provider_id)
-);
-
-CREATE SEQUENCE appointment_serial;
-CREATE TABLE appointments (
-             appointment_id int NOT NULL default nextval ('appointment_serial'),
-             patient_id int,
-             provider_id int,
-             appointment_date DATE,
-             appt_start_time TIME,
-             appt_end_time TIME,
-             status varchar(15) DEFAULT ('Confirmed'),
-             appointment_reason varchar(70),
-             appointment_details varchar(100),
-
-             CONSTRAINT pk_appointments PRIMARY KEY (appointment_id),
-             CONSTRAINT fk_appointments_patient_id FOREIGN KEY (patient_id) REFERENCES patient(patient_id),
-             CONSTRAINT fk_appointments_provider_id FOREIGN KEY (provider_id) REFERENCES provider(provider_id)
-);
-
-
---INSERT INTO users (user_id, username, password_hash, role) VALUES (0, 'user1', 'pass1', patient);
-
---INSERT INTO patient (patient_id, user_id, balance) VALUES (2002, 1002, 1000);
 
 
 COMMIT TRANSACTION;
-
