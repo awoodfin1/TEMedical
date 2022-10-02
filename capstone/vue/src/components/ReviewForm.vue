22a1696ed99b138fed467df4ee5bb287fdc0df40
Aaron Woodfin <awoodfin1@gmail.com>
2022-10-02 17:55:04 -0400

    Formed bare bones on Review service, component and view

---
+++ capstone/vue/src/components/ReviewForm.vue
@@ -0,0 +1,5 @@
+<template>
+    <form v-on:submit.prevent="submitForm" class="reviewForm">
+
+    </form>
+</template>
---
+++ capstone/vue/src/views/Review.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="review">
+        <h1>Add a Review</h1>
+        <p>How was your care?</p>
+        <review-form/>
+
+    </div>
+</template>
+
+<script>
+import ReviewForm from '../components/ReviewForm.vue';
+
+export default {
+    name: "review",
+    components: {
+        ReviewForm
+    },
+}
+</script>
