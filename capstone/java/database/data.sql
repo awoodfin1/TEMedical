a9315dd50d1f7dedbad583b26f5fa159af2c3195
Lucas Arroyo <laarroyo22@gmail.com>
2022-10-06 15:15:32 -0400

    Updated data.sql

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -30,7 +30,7 @@ INSERT INTO users (username, password_hash, "role", is_provider)
 	
 INSERT INTO provider (user_id, title, first_name, last_name, post_nominals, specialty, gender, "language", rating, phone_number, bio, photo_URL)
 	VALUES
-		(100004, 'Dr.', 'Derek', 'Thompson', 'MD', 'Family Practice', 'Male', 'English, Spanish, Chinese', 4.55, '832-736-2635', 'Family practioner that specializes in overall family medicine of all ages.', 'https://as2.ftcdn.net/v2/jpg/03/20/52/31/1000_F_320523164_tx7Rdd7I2XDTvvKfz2oRuRpKOPE5z0ni.jpg');
+		(100004, 'Dr.', 'Derek', 'Thompson', 'MD', 'Family Practice', 'Male', 'English, Spanish, Chinese', 4.55, '832-736-2635', 'Family practioner that specializes in overall family medicine of all ages.', 'https://thumbs.dreamstime.com/b/portrait-positive-black-doctor-holding-medical-chart-male-over-white-background-178499631.jpg');
 
 INSERT INTO users (username, password_hash, "role", is_provider)
 	VALUES
@@ -38,7 +38,7 @@ INSERT INTO users (username, password_hash, "role", is_provider)
 	
 INSERT INTO provider (user_id, title, first_name, last_name, post_nominals, specialty, gender, "language", rating, phone_number, bio, photo_URL)
 	VALUES
-		(100005, 'Dr.', 'Pat', 'Kuhn', 'MD', 'Geriatrics', 'Female', 'English', 4.55, '806-874-2515', 'Medical doctor in community care, specializing in seniors.', 'https://images.squarespace-cdn.com/content/v1/53aa5722e4b0ada1a70119f2/1529694594561-7ERUUT9270RZI2R946J1/CORP20180521-Freeman+%284+of+6%29-Edit.jpg?format=1500w');
+		(100005, 'Dr.', 'Pat', 'Kuhn', 'MD', 'Geriatrics', 'Female', 'English', 4.55, '806-874-2515', 'Medical doctor in community care, specializing in seniors.', 'https://t4.ftcdn.net/jpg/03/17/85/49/360_F_317854905_2idSdvi2ds3yejmk8mhvxYr1OpdVTrSM.jpg');
 
 INSERT INTO users (username, password_hash, "role", is_provider)
 	VALUES
@@ -46,7 +46,7 @@ INSERT INTO users (username, password_hash, "role", is_provider)
 	
 INSERT INTO provider (user_id, title, first_name, last_name, post_nominals, specialty, gender, "language", rating, phone_number, bio, photo_URL)
 	VALUES
-		(100006, 'Mr.', 'Barry', 'Hartford', 'NP', 'Oncology', 'Male', 'English', 4.55, '806-681-1122', 'Nurse specialty of holistic colon cancer care planning and treatments.', 'https://as2.ftcdn.net/v2/jpg/03/20/52/31/1000_F_320523164_tx7Rdd7I2XDTvvKfz2oRuRpKOPE5z0ni.jpg');
+		(100006, 'Mr.', 'Barry', 'Hartford', 'NP', 'Oncology', 'Male', 'English', 4.55, '806-681-1122', 'Nurse specialty of holistic colon cancer care planning and treatments.', 'https://cdn.create.vista.com/api/media/small/242085490/stock-photo-close-up-portrait-of-a');
 		
 INSERT INTO users (username, password_hash, "role", is_provider)
 	VALUES
@@ -54,7 +54,7 @@ INSERT INTO users (username, password_hash, "role", is_provider)
 
 INSERT INTO provider (user_id, title, first_name, last_name, post_nominals, specialty, gender, "language", rating, phone_number, bio, photo_URL)
 	VALUES
-		(100007, 'Dr.', 'Abel', 'Garcia', 'MD', 'Gastroenterologist', 'Male', 'English, Spanish, Arabic', 4.55, '832-886-6717', 'Specializing in colorectal surgery.', 'https://as2.ftcdn.net/v2/jpg/03/20/52/31/1000_F_320523164_tx7Rdd7I2XDTvvKfz2oRuRpKOPE5z0ni.jpg');
+		(100007, 'Dr.', 'Abel', 'Garcia', 'MD', 'Gastroenterologist', 'Male', 'English, Spanish, Arabic', 4.55, '832-886-6717', 'Specializing in colorectal surgery.', 'https://t3.ftcdn.net/jpg/02/97/95/66/360_F_297956690_OMymV1MsaWkAPYOeVVLsR6B5QD1jnp62.jpg');
 		
 INSERT INTO users (username,password_hash,role,is_provider)
 	VALUES
@@ -82,6 +82,15 @@ INSERT INTO patient (user_id, first_name, last_name, phone_number, email_address
 
 INSERT INTO appointment (patient_id, provider_id, appointment_date, appt_start_time, appt_end_time, appointment_reason, appointment_details, is_new_patient)
 	VALUES (2001, 1001, '2022-10-31', '12:00:00', '12:30:00', 'Other', 'Referred by my primary care physician after my trip to the hospital last week.', true);
+	
+INSERT INTO appointment (patient_id, provider_id, appointment_date, appt_start_time, appt_end_time, appointment_reason, appointment_details, is_new_patient)
+	VALUES (2001, 1001, '2022-10-31', '09:00:00', '09:30:00', 'Other', 'Yearly checkup.', true);
+	
+INSERT INTO appointment (patient_id, provider_id, appointment_date, appt_start_time, appt_end_time, appointment_reason, appointment_details, is_new_patient)
+	VALUES (2002, 1001, '2022-10-31', '10:00:00', '10:30:00', 'Other', 'Yearly checkup.', false);
+	
+INSERT INTO appointment (patient_id, provider_id, appointment_date, appt_start_time, appt_end_time, appointment_reason, appointment_details, is_new_patient)
+	VALUES (2003, 1001, '2022-10-31', '11:00:00', '11:30:00', 'Other', 'Yearly checkup.', true);
 
 INSERT INTO office (office_name, tag_line, street_address, city, "state", zipcode, office_open_time, office_close_time, phone_number, cost_per_hour, "message")
 	VALUES
