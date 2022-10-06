cf71fb0726af03145a88b2776cf83068693a587c
Trey Thompson <trey.thompson413@gmail.com>
2022-10-06 15:57:33 -0500

    Provider profile rating is now rounded to 2 decimal places.

--- capstone/vue/src/components/MyInfoProvider.vue
+++ capstone/vue/src/components/MyInfoProvider.vue
@@ -4,7 +4,7 @@
             <h4 class="top-line">{{provider.title}} {{provider.firstName}} {{provider.lastName}} {{provider.postNominals}}</h4>
             <img class="provider-pic" v-bind:src="provider.photoUrl" alt="">
             <h4>{{provider.specialty}}</h4>
-            <h4 v-if="provider.rating">Provider Rating: {{provider.rating}}</h4>
+            <h4 v-if="provider.rating">Provider Rating: {{roundedRating(provider.rating)}}</h4>
             <h4 v-if="provider.phoneNumber">Contact Info:</h4>
             <h4 v-if="provider.phoneNumber">{{provider.phoneNumber}}</h4>
             <h4>This provider speaks {{provider.language}}.</h4>
@@ -28,5 +28,11 @@
             });
             
         },
+        methods: {
+            roundedRating(rating) {
+                rating = Number.parseFloat(rating).toFixed(2);
+                return rating;
+            }
+        }
     }
 </script>
